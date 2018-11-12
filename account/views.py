0ab1eaba6cde4d344a723aa5a275d6da352707e2
David Mares <damr18@gmail.com>
2018-11-12 10:20:31 -0600

    creating profile model for the user

--- account/views.py
+++ account/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, ProfileEditForm,UserEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
+
+
+
+@login_required
+def edit(request):
+  if request.method == 'POST':
+    user_form = UserEditForm(instance=request.user,data=request.POST)
+    profile_form = ProfileEditForm(instance=request.user.profile,data=request.POST,files=request.FILES)
+    if user_form.is_valid() and profile_form.is_valid():
+      user_form.save()
+      profile_form.save()
+  else:
+    user_form = UserEditForm(instance=request.user)
+    profile_form = ProfileEditForm(instance=request.user.profile)
+
+  return render(request,'account/edit.html', {'user_form':user_form,'profile_form':profile_form})
+
+
+
 
 
 @login_required
 def dashboard(request):
-  return render(request, 'account/dashboard.html', {'section':'dashboard'})
+  profile = Profile.objects.get(pk=request.user.profile.id)
+  return render(request, 'account/dashboard.html', {'section':'dashboard','profile':profile})
   
 
 
@@ -21,6 +42,8 @@ def register(request):
       new_user.set_password(user_form.cleaned_data['password'])
       # Save the user object
       new_user.save()
+      #create the user profile
+      Profile.objects.create(user=new_user)
       return render(request,'account/register_done.html',{'new_user':new_user})
   else:
     user_form = UserRegistrationForm()
