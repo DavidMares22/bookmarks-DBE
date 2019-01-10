dde141459961ffa65457e16370a807af1a229fa1
David Mares <damr18@gmail.com>
2019-01-09 21:54:39 -0600

    follow fuctionality

--- account/views.py
+++ account/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate,login
 from django.contrib.auth.models import User
 from .forms import LoginForm, UserRegistrationForm, ProfileEditForm,UserEditForm
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+from .models import Profile,Contact
 from django.contrib import messages
 
 
 
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+  user_id = request.POST.get('id')
+  action = request.POST.get('action')
+  if user_id and action:
+    try:
+      user = User.objects.get(id=user_id)
+      if action == 'follow':
+        Contact.objects.get_or_create(user_from=request.user,user_to=user)
+      else:
+        Contact.objects.filter(user_from=request.user, user_to=user).delete()
+      
+      return JsonResponse({'status':'ok'})
+    except User.DoesNotExist:
+      return JsonResponse({'status':'ko'})
+  return JsonResponse({'status':'ko'})
 
 @login_required
 def user_list(request):
